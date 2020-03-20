6ec6983cf311db515af9318fa9d27bacbf5875fc
Paulo202005 <paulo.argenton@gmail.com>
2020-03-20 18:13:53 -0300

    Herança

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -17,10 +17,13 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    pass
+
 
 if __name__ == '__main__':
-    paulo = Pessoa(nome='Paulo')
-    luciano = Pessoa(paulo, nome='Luciano')
+    paulo = Homem(nome='Paulo')
+    luciano = Homem(paulo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -41,3 +44,8 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos), id(luciano.olhos), id(paulo.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(paulo, Pessoa))
+    print(isinstance(paulo, Homem))
